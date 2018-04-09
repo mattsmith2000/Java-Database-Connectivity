2e82fffaf6a5f27d30c9cb40d1989394df3cde0e
mattsmith2000 <mattsmith2000@aol.com>
2018-04-08 21:42:53 -0400

    Removed SQL Script

--- CREATE_USERS.sql
+++
@@ -1,6 +0,0 @@
-USE travel;
-DROP TABLE IF EXISTS users;
-CREATE TABLE users(username VARCHAR(12) PRIMARY KEY, password VARCHAR(30), name VARCHAR(25), access VARCHAR(7));
-INSERT INTO users VALUES('pef468', 'pass123', 'Pattric Fern', 'Admin');
-INSERT INTO users VALUES('jer164', 'bestpass123', 'Jason Richards', 'Editor');
-INSERT INTO users VALUES('jpc462', 'dpdh', 'Jerry Cantrell', 'General');
